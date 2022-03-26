c905e6bb5c43d0f78c42633e131a5c5efc9833cc
eren <erndemir.1@gmail.com>
2022-03-26 19:06:00 +0300

    commit

--- lib/views/pages/my_orders/my_orders_view_model.dart
+++ lib/views/pages/my_orders/my_orders_view_model.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_order_management/core/services/socket/socket_config.dart';
+import 'package:flutter_order_management/data/models/order_models/order_model.dart';
+import 'package:flutter_order_management/data/sources/database/local_database_helper.dart';
 import 'package:flutter_order_management/views/pages/my_orders/bloc/my_orders_bloc.dart';
 import 'package:flutter_order_management/views/pages/my_orders/my_orders.dart';
 
@@ -10,6 +15,18 @@ abstract class MyOrdersViewModel extends State<MyOrders> with MyOrdersResources 
     super.initState();
     myOrdersBloc = MyOrdersBloc();
     myOrdersBloc?.add(MyOrdersProcessStartEvent());
+    socket = SocketConfig.socket;
+    socket?.on('updateOrderResponse', (val) {
+      Map<String, dynamic> jsonVal = val;
+      OrderModel model = OrderModel.fromJson(jsonVal);
+      log('Result => $model', name: "updateOrderResponse");
+      if (model.userId == LocaleDatabaseHelper.i.currentUserId) {
+        var index = allOrders.indexWhere((element) => element.orderId == model.orderId);
+        setState(() {
+          allOrders[index] = model;
+        });
+      }
+    });
   }
 
   @override
